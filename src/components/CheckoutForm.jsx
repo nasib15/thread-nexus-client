cdbc665653eea6efa70369f40992ba373fa7b43d
knh-nehal <nasibhossain57@gmail.com>
2024-06-05 18:44:39 +0600

    fully functional payment system

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,11 +1,27 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import useAxios from "./../hooks/useAxios";
 
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
   const [error, setError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const axiosFetch = useAxios();
+  const price = 10;
+
+  useEffect(() => {
+    axiosFetch
+      .post("/create-payment-intent", {
+        price,
+      })
+      .then((res) => {
+        console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret);
+      });
+  }, [axiosFetch]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -29,10 +45,29 @@ const CheckoutForm = () => {
     if (error) {
       console.log("error", error);
       setError(error.message);
+      setTransactionId("");
     } else {
       console.log("PaymentMethod", paymentMethod);
-      toast.success("Payment successful!");
       setError("");
+    }
+
+    // confirm payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card,
+        },
+      });
+
+    if (confirmError) {
+      console.log("confirmError", confirmError);
+    } else {
+      console.log("paymentIntent", paymentIntent);
+      if (paymentIntent.status === "succeeded") {
+        console.log("transaction id", paymentIntent.id);
+        setTransactionId(paymentIntent.id);
+        toast.success("Payment successful!");
+      }
     }
   };
 
@@ -55,13 +90,19 @@ const CheckoutForm = () => {
         }}
       />
       {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+
       <button
         className="mt-6 w-full py-3 bg-lime-500 text-neutral-800 font-medium rounded-md hover:bg-lime-600 focus:outline-none focus:bg-lime-600"
         type="submit"
-        disabled={!stripe}
+        disabled={!stripe || !clientSecret}
       >
-        Pay Now
+        Pay ${price}
       </button>
+      {transactionId && (
+        <p className="text-green-500 text-sm mt-4">
+          Transaction ID: {transactionId}
+        </p>
+      )}
     </form>
   );
 };
